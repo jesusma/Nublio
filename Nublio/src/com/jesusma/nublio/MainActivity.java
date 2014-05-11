e66c22d69b38312118f32e0f55735443dc6783ee
jesusma <jesusmacc@gmail.com>
2014-05-12 00:48:33 +0200

    Al iniciar la aplicación aparece un botón que al ser pulsado abre la página de login de Dropbox, pidiendo a continuación permiso para que la aplicación acceda a la cuenta del usuario. Cambiadas las app Key y Secret por unas propias de esta aplicación, estableciéndose los permisos de acceso de la aplicación en Dropbox sólo a archivos del tipo ebook. Modificado todo el código de HelloDropbox y eliminado todo el sobrante.

--- Nublio/src/com/jesusma/nublio/MainActivity.java
+++ Nublio/src/com/jesusma/nublio/MainActivity.java
@@ -1,12 +1,6 @@
 package com.jesusma.nublio;
 
-/*
- * Basado en el código de ejmplo helloDropbox 
- * de la Sync API de Dropbox
- * 
- */
-import java.io.IOException;
-import java.util.List;
+
 
 import android.app.Activity;
 import android.content.Intent;
@@ -15,126 +9,82 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dropbox.sync.android.DbxAccountManager;
-import com.dropbox.sync.android.DbxFile;
-import com.dropbox.sync.android.DbxFileInfo;
-import com.dropbox.sync.android.DbxFileSystem;
-import com.dropbox.sync.android.DbxPath;
+
+/*
+ * Basado en el código de ejemplo helloDropbox 
+ * de la SyncAPI de Dropbox
+ * 
+ */
 
 public class MainActivity extends Activity {
 
-    private static final String appKey = "p78hpiyefxd4p9z";
-    private static final String appSecret = "hwjzoa1qoxjoqft";
+	//Inicialización variables de autenticación
+    private static final String appKey = "2s0d8d9owizdne4"; 
+    private static final String appSecret = "kzhnaa85s2clrk8";
 
     private static final int REQUEST_LINK_TO_DBX = 0;
 
-    private TextView mTestOutput;
-    private Button mLinkButton;
     private DbxAccountManager mDbxAcctMgr;
+    private Button mLinkButton;
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mTestOutput = (TextView) findViewById(R.id.test_output);
-        mLinkButton = (Button) findViewById(R.id.link_button);
+    
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//Activity inicial
+        
+        mLinkButton = (Button) findViewById(R.id.button1);
         mLinkButton.setOnClickListener(new OnClickListener() {
+        	
             @Override
             public void onClick(View v) {
-                onClickLinkToDropbox();
+            	
+                onClickLinkToDropbox(); //Muestra la pantalla de login de dropbox
+                
             }
+            
         });
-
+       
         mDbxAcctMgr = DbxAccountManager.getInstance(getApplicationContext(), appKey, appSecret);
+        
     }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		if (mDbxAcctMgr.hasLinkedAccount()) {
-		    showLinkedView();
-		    doDropboxTest();
-		} else {
-			showUnlinkedView();
-		}
-	}
-
-    private void showLinkedView() {
-        mLinkButton.setVisibility(View.GONE);
-        mTestOutput.setVisibility(View.VISIBLE);
-    }
-
-    private void showUnlinkedView() {
-        mLinkButton.setVisibility(View.VISIBLE);
-        mTestOutput.setVisibility(View.GONE);
-    }
-
+    //Mostrar pantalla login Dropbox
+    
     private void onClickLinkToDropbox() {
+    	
         mDbxAcctMgr.startLink((Activity)this, REQUEST_LINK_TO_DBX);
+     
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	
         if (requestCode == REQUEST_LINK_TO_DBX) {
+        	
             if (resultCode == Activity.RESULT_OK) {
-                doDropboxTest();
+            	
+            	Toast.makeText(this, "Link to your Dropbox account OK", Toast.LENGTH_LONG).show();
+            	Toast.makeText(this, "Reading you ebook library...", Toast.LENGTH_LONG).show();
+            	//Iniciamos la activity que moestrara lista de libros en pantalla
+            	startActivity(new Intent(this, DxLibrary.class));
+             	
             } else {
-                mTestOutput.setText("Link to Dropbox failed or was cancelled.");
+            	
+            	Toast.makeText(this, "Link to Dropbox failed or was cancelled.", Toast.LENGTH_LONG).show();
+                
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    private void doDropboxTest() {
-        mTestOutput.setText("Dropbox Sync API Version "+DbxAccountManager.SDK_VERSION_NAME+"\n");
-        try {
-            final String TEST_DATA = "Hello Dropbox";
-            final String TEST_FILE_NAME = "hello_dropbox.txt";
-            DbxPath testPath = new DbxPath(DbxPath.ROOT, TEST_FILE_NAME);
-
-            // Create DbxFileSystem for synchronized file access.
-            DbxFileSystem dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
-
-            // Print the contents of the root folder.  This will block until we can
-            // sync metadata the first time.
-            List<DbxFileInfo> infos = dbxFs.listFolder(DbxPath.ROOT);
-            mTestOutput.append("\nContents of app folder:\n");
-            for (DbxFileInfo info : infos) {
-                mTestOutput.append("    " + info.path + ", " + info.modifiedTime + '\n');
-            }
-
-            // Create a test file only if it doesn't already exist.
-            if (!dbxFs.exists(testPath)) {
-                DbxFile testFile = dbxFs.create(testPath);
-                try {
-                    testFile.writeString(TEST_DATA);
-                } finally {
-                    testFile.close();
-                }
-                mTestOutput.append("\nCreated new file '" + testPath + "'.\n");
-            }
-
-            // Read and print the contents of test file.  Since we're not making
-            // any attempt to wait for the latest version, this may print an
-            // older cached version.  Use getSyncStatus() and/or a listener to
-            // check for a new version.
             
-            if (dbxFs.isFile(testPath)) {
-                String resultData;
-                DbxFile testFile = dbxFs.open(testPath);
-                try {
-                    resultData = testFile.readString();
-                } finally {
-                    testFile.close();
-                }
-                mTestOutput.append("\nRead file '" + testPath + "' and got data:\n    " + resultData);
-            } else if (dbxFs.isFolder(testPath)) {
-                mTestOutput.append("'" + testPath.toString() + "' is a folder.\n");
-            }
-        } catch (IOException e) {
-            mTestOutput.setText("Dropbox test failed: " + e);
+        } else {
+        	
+            super.onActivityResult(requestCode, resultCode, data);
+            
         }
     }
+
+   
 }
